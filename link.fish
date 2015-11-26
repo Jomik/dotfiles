5267b0fb64c0a1eee41e2de19297ee6d5466bc51
Jonas Damtoft <Jomik@users.noreply.github.com>
2015-11-26 17:42:48 +0100

    Commented link.fish, added 'strip' option

--- link.fish
+++ link.fish
@@ -7,26 +7,40 @@ function message
     set -l msg $argv[1]
     set -l color $argv[2]
     set_color $color
-    echo $msg
+    echo -e $msg
     set_color normal
 end
 
 # linkFiles $src $dist $depth $filter $prefix
+# $src Decides the relative source to $dotfiles.
+# $dist is the root folder to create links to.
+# $depth is the depth to use with find, 1 is only the $src folder.
+# $filter is the filter to use when selecting files to source. Useful if some
+# files should be ignored.
+# $prefix is the prefix to prepend on every symlinked file. Let's us keep repo
+# files without the dot.
+# $strip is used to strip a suffix from a file, useful together with filter.
+#
+# Example:
+# linkFiles $dotfiles/home ~ 1 "*.symlink" "." ".symlink"
+# Only select files in home that is appended with symlink, then strip it from
+# them and prepend a dot when linking.
 function linkFiles
     set -l src $argv[1]
     set -l dist $argv[2]/
     set -l depth $argv[3]
     set -l filter $argv[4]
     set -l prefix $argv[5]
+    set -l strip $argv[6]
 
-    echo -e "\nCreating symlinks for $dist"
-    echo "=================================================="
+    message "\nCreating symlinks for $dist" blue
+    message "==================================================" blue
 
     for file in (find -H $src -maxdepth $depth -name $filter)
         # Test if it is a file.
         if test -f $file
             # Strip the path and prepend our prefix.
-            set -l targetFile {$prefix}(basename $file)
+            set -l targetFile {$prefix}(basename $file $strip)
             # Strip off the source path so we can prepend distination path.
             set -l targetDir {$dist}(echo $file | sed 's|^'$src'/||')
             # Strip off the file part so we can append targetFile
@@ -75,8 +89,8 @@ function linkFiles
     end
 end
 
-linkFiles $dotfiles/home ~ 3 "*" "." 
-linkFiles $dotfiles/config ~/.config 2 "*" ""
-linkFiles $dotfiles/vim ~/.vim 2 "*" ""
-linkFiles $dotfiles/xmonad ~/.xmonad 1 "*" ""
-linkFiles $dotfiles/weechat ~/.weechat 3 "*" ""
+linkFiles $dotfiles/home ~ 1 "*" "." ""
+linkFiles $dotfiles/config ~/.config 2 "*" "" ""
+linkFiles $dotfiles/vim ~/.vim 2 "*" "" ""
+linkFiles $dotfiles/xmonad ~/.xmonad 1 "*" "" ""
+linkFiles $dotfiles/weechat ~/.weechat 3 "*" "" ""
