18e329a07821203071a8e149573b32f63c0badce
Jonas Damtoft <Jomik@users.noreply.github.com>
2015-12-01 01:42:23 +0100

    More emacs

--- home/emacs.el
+++ home/emacs.el
@@ -13,8 +13,7 @@
 (require 'use-package)
 (use-package use-package-chords
   :ensure t
-  :config
-  (key-chord-mode 1))
+  :config (key-chord-mode 1))
 
 ;; Global settings
 ;(menu-bar-mode -1)
@@ -23,15 +22,49 @@
 
 (set-default-font "Knack 10")
 
+;; Set up a file for the customization system
+(setq custom-file "~/.emacs.d/etc/custom.el")
+(load custom-file)
+
+;; Change backup and autosave settings
+(setq backup-directory-alist '((".*" . "~/.emacs.d/backup")))
+(setq version-control t)
+(setq delete-old-versions t)
+(setq auto-save-list-file-prefix "~/.emacs.d/autosave/")
+(setq auto-save-file-name-transforms '((".*" "~/.emacs.d/autosave/" t)))
+
+;; Disable ads
+(setq inhibit-startup-screen t)
+(defun display-startup-echo-area-message ()
+  (message "Let the hacking begin!"))
+
+;; Make use of our memory. Garbage collection happens at 50MiB
+(setq gc-cons-threshold 50000000)
+
+;; Save place in file
+(setq-default save-place t)
+(setq save-place-file "~/.emacs.d/etc/saveplace")
+
 (use-package solarized-theme
   :ensure t
+  :config (load-theme 'solarized-light t))
+
+;; Better M-x
+(use-package smex
+  :ensure t
   :config
-  (load-theme 'solarized-light t))
+  (smex-initialize)
+  :bind (("M-x" . smex)
+    ("M-X" . smex-major-mode-commands)
+    ("C-c C-c M-x" . execute.extended-command)))
+
+(use-package org
+  :ensure t
+  :config (add-to-list 'auto-mode-alist '("\\.org$" . org-mode)))
 
 (use-package evil
   :ensure t
-  :init
-  (progn
+  :init (progn
     (use-package evil-leader
       :ensure t
       :init
@@ -39,4 +72,5 @@
       :config
       (evil-leader/set-leader ",")
       (global-evil-leader-mode))
-    (evil-mode t)))
+    (evil-mode t))
+  :chords ("jk" . evil-normal-state))
