37319f2e73a49dca4ef70212241194e4fc578750
Jonas Damtoft <Jomik@users.noreply.github.com>
2016-06-15 14:09:44 +0200

    Old

--- home/emacs.el
+++ home/emacs.el
@@ -224,8 +224,48 @@
 
 (use-package scheme
   :ensure nil
+  :defer t
   :config
-  (setq scheme-program-name "petite")) 
+  (setq scheme-program-name "petite")
+  (defun scheme-send-buffer-and-go ()
+    "Send entire content of the buffer to the Inferior Scheme process\
+   and goto the Inferior Scheme buffer."
+    (interactive)
+    (scheme-send-region-and-go (point-min) (point-max)))
+  (add-hook 'scheme-mode-hook
+            (lambda ()
+              (local-set-key (kbd "C-c C-b") 'scheme-send-buffer-and-go)
+              ;; fix indentation of some special forms
+              (put 'cond   'scheme-indent-hook 0)
+              (put 'guard  'scheme-indent-hook 1)
+              (put 'when   'scheme-indent-hook 1)
+              (put 'unless 'scheme-indent-hook 1)
+              ;; special forms from Petite Chez Scheme
+              (put 'trace-lambda  'scheme-indent-hook 2)
+              (put 'extend-syntax 'scheme-indent-hook 1)
+              (put 'with          'scheme-indent-hook 0)
+              (put 'parameterize  'scheme-indent-hook 0)
+              (put 'define-syntax 'scheme-indent-hook 1)
+              (put 'syntax-case   'scheme-indent-hook 0)
+              ;; special forms for Schelog
+              (put '%rel   'scheme-indent-hook 1)
+              (put '%which 'scheme-indent-hook 1)
+              )) 
+  (add-hook 'inferior-scheme-mode-hook
+            (lambda ()
+              ;; Overwrite the standard 'switch-to-buffer' to use
+              ;; 'switch-to-buffer-other-window'
+              (defun switch-to-scheme (eob-p)
+                "Switch to the scheme process buffer.
+     With argument, position cursor at end of buffer."
+                (interactive "P")
+                (if (or (and scheme-buffer (get-buffer scheme-buffer))
+                        (scheme-interactively-start-process))
+                    (switch-to-buffer-other-window scheme-buffer)
+                  (error "No current process buffer.  See variable `scheme-buffer'"))
+                (when eob-p
+                  (push-mark)
+                  (goto-char (point-max))))))) 
 
 ;; Tool for writing LaTeX
 (use-package tex-site
