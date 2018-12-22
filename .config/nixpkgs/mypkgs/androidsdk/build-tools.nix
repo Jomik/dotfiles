31461e281b9c500f184c08bc303ffc0735a2fb7b
Jonas Holst Damtoft <jonasdamtoft@hotmail.com>
2018-12-22 23:17:56 +0100

    android: fix build-tools

--- .config/nixpkgs/mypkgs/androidsdk/build-tools.nix
+++ .config/nixpkgs/mypkgs/androidsdk/build-tools.nix
@@ -13,7 +13,7 @@ in stdenv.mkDerivation rec {
     inherit sha256;
     url = "https://dl.google.com/android/repository/build-tools_r${version}-linux.zip";
   };
-  buildInputs = with pkgs; [ unzip ];
+  buildInputs = with pkgs; [ unzip file ];
 
   dontBuild = true;
 
@@ -29,25 +29,25 @@ in stdenv.mkDerivation rec {
   preFixup = ''
     prefix=$out/build-tools/${version}
     libdir=$out/libs/${version}
+
     for f in $(grep -Rl /bin/ls $prefix); do
       sed -i -e "s|/bin/ls|${pkgs.coreutils}/bin/ls|" "$f"
     done
 
-    for file in aapt aapt2 aidl zipalign split-select llvm-rs-cc bcc_compat dexdump x86_64-linux-android-ld; do
-      if [ -f $prefix/$file ]; then
-        patchelf \
-          --set-interpreter "${stdenv.cc.libc.out}/lib/ld-linux-x86-64.so.2" \
-          --set-rpath ${stdenv.cc.cc.lib.out}/lib:${pkgs.zlib.out}/lib:${pkgs.ncurses5.out}/lib:$libdir/lib64 \
-            $prefix/$file
-      fi
-    done
-
-    for file in aarch64-linux-android-ld arm-linux-androideabi-ld i686-linux-android-ld mipsel-linux-android-ld; do
-      if [ -f $prefix/$file ]; then
-        patchelf \
-          --set-interpreter "${stdenv_32bit.cc.libc.out}/lib/ld-linux.so.2" \
-          --set-rpath ${stdenv_32bit.cc.cc.lib.out}/lib:${zlib_32bit.out}/lib:${ncurses_32bit.out}/lib:$libdir/lib \
-            $prefix/$file
+    find $prefix -type f -print0 | while IFS= read -r -d "" file; do
+      type=$(file "$file")
+      ## Patch 64-bit binaries
+      if grep -q "ELF 64-bit" <<< "$type"; then
+        if grep -q "interpreter" <<< "$type"; then
+          patchelf --set-interpreter ${stdenv.cc.libc.out}/lib/ld-linux-x86-64.so.2 "$file"
+        fi
+        patchelf --set-rpath $libdir/lib64:${stdenv.cc.cc.lib.out}/lib:${pkgs.zlib.out}/lib:${pkgs.ncurses5.out}/lib "$file"
+      ## Patch 32-bit binaries
+      elif grep -q "ELF 32-bit" <<< "$type"; then
+        if grep -q "interpreter" <<< "$type"; then
+          patchelf --set-interpreter ${stdenv_32bit.cc.libc.out}/lib/ld-linux.so.2 "$file"
+        fi
+        patchelf --set-rpath $libdir/lib:${stdenv_32bit.cc.cc.lib.out}/lib:${zlib_32bit.out}/lib:${ncurses_32bit.out}/lib "$file"
       fi
     done
   '';
