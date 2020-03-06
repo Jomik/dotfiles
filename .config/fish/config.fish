61875d69dbb2175b30baa98723cc7eb9f1cc9da6
Jonas Holst Damtoft <jonasdamtoft@gmail.com>
2020-03-06 12:55:39 +0100

    fix sourcing host fish file

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,6 @@
-source (hostname).fish
+set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+
+source $XDG_CONFIG_HOME/fish/(hostname).fish
 
 abbr -a ls exa
 abbr -a ll exa -lha
@@ -8,7 +10,6 @@ abbr -a grep rg
 abbr -a cat bat
 
 if not functions -q fisher
-    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
     curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
     fish -c fisher
 end
