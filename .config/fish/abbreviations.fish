0a6ee8d8f7bacc3a7f678c45c719721e4926d68e
Jonas Holst Damtoft <jonasdamtoft@gmail.com>
2023-03-25 16:30:24 +0100

    fish: update plugins

--- .config/fish/abbreviations.fish
+++ .config/fish/abbreviations.fish
@@ -4,10 +4,3 @@ abbr -a lt exa --tree
 abbr -a psg 'ps aux | rg -v rg | rg -i -e VSZ -e'
 abbr -a grep rg
 abbr -a cat bat
-
-function multicd
-    echo cd (string repeat -n (math (string length -- $argv[1]) - 1) ../)
-end
-
-abbr -a dotdot --regex '^\.\.+$' --function multicd
-
