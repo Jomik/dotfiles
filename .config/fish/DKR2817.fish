9750c0bac3ac9fd99bd40820286a2cf2bbfd90ba
Jonas Holst Damtoft <jonasdamtoft@gmail.com>
2023-04-10 11:46:27 +0200

    fish: update PATH

--- .config/fish/DKR2817.fish
+++ .config/fish/DKR2817.fish
@@ -3,8 +3,8 @@ set -x GOPATH $HOME/go
 
 status --is-interactive; and source (rbenv init -|psub)
 
-fish_add_path ~/.cargo/bin
+fish_add_path $HOME/.cargo/bin
 fish_add_path $GOPATH/bin
+fish_add_path $HOME/.local/bin
 
 abbr -a lg lazygit
-
