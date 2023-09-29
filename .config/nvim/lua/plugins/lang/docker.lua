5a99f42c9263841b9c679b99d943a4e805695842
Jonas Holst Damtoft <jonasdamtoft@gmail.com>
2023-09-29 16:31:00 +0200

    nvim: update docker plugin

--- .config/nvim/lua/plugins/lang/docker.lua
+++ .config/nvim/lua/plugins/lang/docker.lua
@@ -1,28 +1,3 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = function(_, opts)
-      if type(opts.ensure_installed) == "table" then
-        vim.list_extend(opts.ensure_installed, { "dockerfile" })
-      end
-    end,
-  },
-  {
-    "nvimtools/none-ls.nvim",
-    opts = function(_, opts)
-      local nls = require("null-ls")
-      vim.list_extend(opts.sources, {
-        nls.builtins.diagnostics.hadolint,
-      })
-    end,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        dockerls = {},
-        docker_compose_language_service = {},
-      },
-    },
-  },
+  { import = "lazyvim.plugins.extras.lang.docker" },
 }
