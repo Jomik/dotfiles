6bc855735e0fbc672008b7ff899308d9732f3e13
Jonas Holst Damtoft <jonasdamtoft@gmail.com>
2023-10-05 12:30:13 +0200

    nvim: use lazyvim folding

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -9,10 +9,6 @@ local opt = vim.opt
 
 opt.wrap = true
 opt.linebreak = true
-opt.foldlevel = 99
-opt.foldlevelstart = 99
-opt.foldenable = true
-opt.foldcolumn = "0"
 
 if vim.g.neovide then
   opt.guifont = "VictorMono Nerd Font Mono-SemiBold:h14"
