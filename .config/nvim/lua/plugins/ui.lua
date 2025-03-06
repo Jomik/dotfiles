ac59e89bba01bc015f1f274fce26aead94e72a63
Jonas Damtoft <Jomik@users.noreply.github.com>
2025-03-06 18:50:15 +0100

    nvim: fix neotree and edgy integration

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -14,4 +14,11 @@ return {
       manual_mode = false,
     },
   },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    optional = true,
+    opts = {
+      open_files_do_not_replace_types = { "edgy" },
+    },
+  },
 }
